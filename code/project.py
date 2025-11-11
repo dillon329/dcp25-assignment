b7cd9804cb3bb3f5ebd16cf2574a2b52771d3409
dillon329 <c24452624@mytudublin.ie>
2025-11-11 14:31:54 +0000

    Sort Through abc files(not function)

--- code/project.py
+++ code/project.py
@@ -3,10 +3,41 @@ def loadabcFiles(filename):
         lines = f.readlines()  
     return lines
 
+tunes = []
+
+lines = loadabcFiles("abc_books/2/00.abc")
+title_check = False
+
+for line in lines:
+    # Check if this starts a new tune
+    line = line.strip()
+
+    if line.startswith("X:"):
+        current_tune_lines = {
+            "id": int(line[2:]),
+            "title": None,
+            "alt_title": None,
+            "tune": None,
+            "Key": None,
+            
+        }
+        title_check = False
+
+    elif line.startswith("T:"):
+        title = line[2:].strip()
+        if not title_check:
+            current_tune_lines['title'] = title
+            title_check = True
+        else:
+            current_tune_lines['alt_title'] = title
+
+    elif line.startswith("R:"):
+        current_tune_lines['tune'] = line[2:].strip()
+    elif line.startswith("K:"):
+        current_tune_lines['Key'] = line[2:].strip()
+        tunes.append(current_tune_lines.copy())
 
 
+print(lines[:20])
 
-df = loadabcFiles("abc_books/2/00.abc")
-
-
-print(df[:20])
+print(tunes)

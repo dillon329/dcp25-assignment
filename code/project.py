da23d508a69a5b97cdd7cfb779cbe53794d8c5bb
dillon329 <c24452624@mytudublin.ie>
2025-11-13 00:11:57 +0000

    Getting rid of old file system

--- code/project.py
+++ code/project.py
@@ -1,20 +1,21 @@
 from collections import Counter
-
-def loadabcFiles(filename):
-    with open(filename, "r", encoding='latin-1') as f:
-        lines = f.readlines()  
-    return lines
-
-
-
-
+import os
 
 def cleanFiles(lines):
-    global tunes 
+    current_tune_lines = {
+                "id": None,
+                "title": None,
+                "alt_title": '',
+                "tune": None,
+                "Key": None,
+                
+            }
+    tunes = []
     #not all lines end with the same line, thus i added this to make it so that after the for loop has passed through once
     #that it would append when it comes up on a new X:, and for the last set just to append it when the for loop is finished
     pass_through_1 = False
     for line in lines:
+        
         # Check if this starts a new tune
         line = line.strip()
         if line =='X: 228':
@@ -43,25 +44,41 @@ def cleanFiles(lines):
                 current_tune_lines['alt_title'] = title
 
         elif line.startswith("R:"):
-            current_tune_lines['tune'] = line[2:].strip()
+            try:
+                current_tune_lines['tune'] = line[2:].strip()
+            except:
+                if line.startswith("R:"):
+                    try:
+                        current_tune_lines['tune'] = line[2:].strip()
+                    except:
+                        print("Oh no")
+        
         elif line.startswith("K:"):
             current_tune_lines['Key'] = line[2:].strip()
     tunes.append(current_tune_lines.copy())
-    
+    return tunes
 
 
+def loadabcFiles(folder):
+    global tunes 
+    for foldername in os.listdir(folder):
+        for filename in os.listdir(folder+"/"+foldername):
+            if filename.endswith(".abc"):
+                path = os.path.join(folder, foldername, filename)
+                with open(path, "r", encoding="utf-8") as f:
+                    lines = f.readlines()
+                    tunes.append(cleanFiles(lines))
 
 tunes = []
 
-files = ['abc_books/2/00.abc',"abc_books/2/01.abc", "abc_books/2/02.abc", "abc_books/2/03.abc",
-         "abc_books/2/04.abc", "abc_books/2/05.abc", "abc_books/2/06.abc", "abc_books/2/07.abc",
-         "abc_books/2/08.abc", "abc_books/2/09.abc", "abc_books/2/10.abc"]
+folder = "abc_books"
+                
+loadabcFiles(folder)
 
-for file in files:
-    file = loadabcFiles(file)
-    cleanFiles(file)
-
+print(tunes)
 #parses the sublists in the tunes list
+tunes = [item for sublist in tunes for item in sublist]
+
 
 #checking for dupes in dataset, alter by chatGpt
 seen_titles = set()
--- code/test.py
+++ code/test.py
@@ -1,11 +1,20 @@
 import os
 
 def cleanFiles(lines):
-    global tunes 
+    current_tune_lines = {
+                "id": None,
+                "title": None,
+                "alt_title": '',
+                "tune": None,
+                "Key": None,
+                
+            }
+    tunes = []
     #not all lines end with the same line, thus i added this to make it so that after the for loop has passed through once
     #that it would append when it comes up on a new X:, and for the last set just to append it when the for loop is finished
     pass_through_1 = False
     for line in lines:
+        
         # Check if this starts a new tune
         line = line.strip()
         if line =='X: 228':
@@ -34,25 +43,37 @@ def cleanFiles(lines):
                 current_tune_lines['alt_title'] = title
 
         elif line.startswith("R:"):
-            current_tune_lines['tune'] = line[2:].strip()
+            try:
+                current_tune_lines['tune'] = line[2:].strip()
+            except:
+                if line.startswith("R:"):
+                    try:
+                        current_tune_lines['tune'] = line[2:].strip()
+                    except:
+                        print("Oh no")
+        
         elif line.startswith("K:"):
             current_tune_lines['Key'] = line[2:].strip()
     tunes.append(current_tune_lines.copy())
+    return tunes
 
 
 def loadabcFiles(folder):
+    global tunes 
     for foldername in os.listdir(folder):
         for filename in os.listdir(folder+"/"+foldername):
             if filename.endswith(".abc"):
                 path = os.path.join(folder, foldername, filename)
                 with open(path, "r", encoding="utf-8") as f:
                     lines = f.readlines()
-                    cleanFiles(lines)
+                    tunes.append(cleanFiles(lines))
 
 tunes = []
 
 folder = "abc_books"
                 
 loadabcFiles(folder)
+
+print(tunes)
 
             
